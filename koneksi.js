faf28276ab2a24fbc7da6c7b2cac3556a2eed193
seiga al ghifari <seigaalghi@gmail.com>
2020-10-27 20:26:56 +0700

    membuat koneksi

---
+++ koneksi.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql')
+
+// koneksi ke database
+
+const conn = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'dbrestapi'
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('mysql terkoneksi')
+});
+
+module.exports = conn;
